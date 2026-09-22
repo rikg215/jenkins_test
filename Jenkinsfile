2a1e5b2675a57551741d6b208e5b728ea67c3164
admin <rik215@live.com>
2026-09-22 19:30:35 -0400

    changed Jenkinsfile and groovy.script to enable versioning on each commit

--- Jenkinsfile
+++ Jenkinsfile
@@ -13,6 +13,13 @@ pipeline {
                 }
             }
         }
+        stage("version") {
+            steps {
+                script {
+                    gv.appVersion()
+                }
+            }
+        }
         stage("build jar") {
             steps {
                 script{
--- script.groovy
+++ script.groovy
@@ -1,14 +1,24 @@
+def appVersion() {
+    echo 'updating version...'
+    sh 'mvn build-helper:parse-version versions:set \
+       -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+       versions:commit'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
+
 def buildJar() {
     echo 'building the application'
-    sh 'mvn package'
+    sh 'mvn clean package'
 }
 
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'rik215', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t rik215/bootcamp-test:jma-2.0 .'
+        sh "docker build -t rik215/bootcamp-test:$IMAGE_NAME ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push rik215/bootcamp-test:jma-2.0'
+        sh "docker push rik215/bootcamp-test:$IMAGE_NAME"
                     }
 }
 
