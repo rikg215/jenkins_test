c7022e4f4a055cd9a9dcd6d6aa27efdbfd131d0f
admin <rik215@live.com>
2026-09-23 12:30:54 -0400

    test commit for jenkins

--- script.groovy
+++ script.groovy
@@ -14,7 +14,7 @@ def buildJar() {
 }
 
 def buildImage() {
-    echo "building the docker image..."
+    echo "building the docker image...."
     withCredentials([usernamePassword(credentialsId: 'rik215', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "docker build -t rik215/bootcamp-test:${IMAGE_NAME} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
