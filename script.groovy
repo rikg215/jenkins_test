de13e335e5ebd8b58a868a5a8e5a4747d3b1c294
admin <rik215@live.com>
2026-09-23 11:55:54 -0400

    corrected syntax for variable interpolation

--- script.groovy
+++ script.groovy
@@ -36,7 +36,7 @@ def commitVersion() {
         sh 'git branch'
         sh 'git config --list'
 
-        sh "git remote set-url origin https://${USER}:${PASS}@github.com/rikg215/jenkins_test.git"
+        sh 'git remote set-url origin "https://${USER}:${PASS}@github.com/rikg215/jenkins_test.git"'
         sh 'git add ,'
         sh 'git commit -m "ci: version bump'
         sh 'git push origin HEAD:feature/Dockerfile'
