cee223077a0e3720b12bea306943b3324e690bbc
admin <rik215@live.com>
2026-09-23 11:27:55 -0400

    configure version increment in build

--- script.groovy
+++ script.groovy
@@ -16,9 +16,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'rik215', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t rik215/bootcamp-test:$IMAGE_NAME ."
+        sh "docker build -t rik215/bootcamp-test:${IMAGE_NAME} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push rik215/bootcamp-test:$IMAGE_NAME"
+        sh "docker push rik215/bootcamp-test:${IMAGE_NAME}"
                     }
 }
 
